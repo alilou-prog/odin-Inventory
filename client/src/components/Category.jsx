2ec310f0ef9d59c4f618d262279406cf522b30bc
aliou-prog <alimokhati2023@gmail.com>
2025-07-24 16:05:22 +0100

    update form for each category

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,12 +1,20 @@
-import {fetch_data} from "../data/data"
+import { useState } from "react";
+import { fetch_data } from "../data/data"
 
 export default function Category({ category, set_categories }) {
+    const [updating, set_updating] = useState(false);
+    const start_updating = () => {
+        set_updating(true);
+    }
+
     const update = async (e) => {
+        e.preventDefault();
         const id = parseInt(e.currentTarget.getAttribute('data-id'));
+        const name = e.target.value;
         await fetch(`/api/categories/${id}`, {
-            method: "PUT", 
+            method: "PUT",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ id, name: "updated name" })
+            body: JSON.stringify({ id, name })
         })
         set_categories(await fetch_data())
     }
@@ -20,18 +28,30 @@ export default function Category({ category, set_categories }) {
     const handle_click = (e) => {
         switch (e.target.role) {
             case "update":
-                update(e);
+                start_updating(e);
                 break;
             case "delete":
                 del(e);
+                break;
+            case "sumbit":
+                update(e);
                 break;
             default:
                 throw Error(`Invalid value for e.target.role, expected (update | delete), got ${e.target.role}`)
         }
     }
+    
     return (
         <li data-id={category.id} onClick={handle_click}>
-            <div className="display">name: {category.name}</div>
+            {updating ? (
+                <form>
+                    <label htmlFor="name"></label>
+                    <input type="text" name="name" placeholder={category.name} />
+                    <button className="submit" role="submit">Submit</button>
+                </form>
+            ) :
+                (<div className="display">name: {category.name}</div>)
+            }
             <div className="btns">
                 <button className="update" role="update">Update</button>
                 <button className="delete" role="delete">Delete</button>
